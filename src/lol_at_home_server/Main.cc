f0877098ce9c6fec598d0cf419c537fcc8ecc809
hwabis <kennysun23@gmail.com>
2025-09-16 22:13:04 -0400

    enet

--- src/lol_at_home_server/Main.cc
+++ src/lol_at_home_server/Main.cc
@@ -1,7 +1,12 @@
+#include <enet/enet.h>
 #include <spdlog/spdlog.h>
 #include "GameStateThread.h"
 
 auto main() -> int {
   lol_at_home_server::GameStateThread trololol;
   spdlog::info("i show spd");
+  if (enet_initialize() != 0) {
+    spdlog::error("Failed to initialize ENet");
+    return 1;
+  }
 }
