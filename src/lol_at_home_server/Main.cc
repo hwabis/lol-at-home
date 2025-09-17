a0a451390490ec8f6eef629970b4caa017a04224
hwabis <kennysun23@gmail.com>
2025-09-16 22:07:23 -0400

    i show spd

--- src/lol_at_home_server/Main.cc
+++ src/lol_at_home_server/Main.cc
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
 #include "GameStateThread.h"
 
 auto main() -> int {
-  std::cout << "troll";
   lol_at_home_server::GameStateThread trololol;
+  spdlog::info("i show spd");
 }
