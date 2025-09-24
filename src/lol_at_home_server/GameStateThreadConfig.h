23c6d155f282d83ff350637a6d8cc46430ada301
hwabis <kennysun23@gmail.com>
2025-09-23 22:04:13 -0400

    R U HAPPY NAO OMG

--- src/lol_at_home_server/GameStateThreadConfig.h
+++ src/lol_at_home_server/GameStateThreadConfig.h
@@ -5,10 +5,15 @@
 namespace lol_at_home_server {
 
 struct GameStateThreadConfig {
-  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-  std::chrono::milliseconds UpdateInterval = std::chrono::milliseconds{33};
-  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-  std::chrono::seconds FullStateBroadcastInterval = std::chrono::seconds{5};
+ public:
+  std::chrono::milliseconds UpdateInterval =
+      std::chrono::milliseconds{updateInterval};
+  std::chrono::seconds FullStateBroadcastInterval =
+      std::chrono::seconds{fullStateBroadcastInterval};
+
+ private:
+  static constexpr int updateInterval = 33;
+  static constexpr int fullStateBroadcastInterval = 33;
 };
 
 }  // namespace lol_at_home_server
